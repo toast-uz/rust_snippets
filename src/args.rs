d667a47a9ca6f1a3231c1183f607b00391718a38
toast-uz <tatsuzo.osawa@gmail.com>
2023-09-02 07:26:50 +0900

    Replace main position

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,29 @@
-use regex::Regex;
-use itertools::Itertools;
+
+mod kyopro_args {
+    use regex::Regex;
+    use itertools::Itertools;
+
+    pub struct Args {
+        args_str: String,
+    }
+
+    impl Args {
+        pub fn new() -> Self {
+            let args: Vec<String> = std::env::args().collect();
+            let args_str = args[1..].iter().join(" ");
+            Self { args_str }
+        }
+
+        pub fn get_num<T: std::str::FromStr>(&self, arg_name: &str) -> Option<T> {
+            let re_str = format!(r"-{}=([\d.-]+)", arg_name);
+            let re = Regex::new(&re_str).unwrap();
+            let Some(captures) = re.captures(&self.args_str) else { return None; };
+            captures[1].parse().ok()
+        }
+    }
+}
+
+use kyopro_args::Args;
 
 fn main() {
     let args = Args::new();
@@ -8,21 +32,3 @@ fn main() {
     println!("x: {}, y: {}", x, y);
 }
 
-struct Args {
-    args_str: String,
-}
-
-impl Args {
-    fn new() -> Self {
-        let args: Vec<String> = std::env::args().collect();
-        let args_str = args[1..].iter().join(" ");
-        Self { args_str }
-    }
-
-    fn get_num<T: std::str::FromStr>(&self, arg_name: &str) -> Option<T> {
-        let re_str = format!(r"-{}=([\d.-]+)", arg_name);
-        let re = Regex::new(&re_str).unwrap();
-        let Some(captures) = re.captures(&self.args_str) else { return None; };
-        captures[1].parse().ok()
-    }
-}
