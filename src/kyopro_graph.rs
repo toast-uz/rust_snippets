7a6609eeecf284181a657c74bb9e5dc3d730a4b6
toast-uz <tatsuzo.osawa@gmail.com>
2023-09-03 10:05:29 +0900

    1st completely Commit

--- src/kyopro_graph.rs
+++ src/kyopro_graph.rs
@@ -5,32 +5,24 @@ use rustc_hash::FxHashSet as HashSet;
 
 const INF: usize = 1e18 as usize;
 
-#[derive(Debug, Clone)]
-struct Map<T, C> {
+#[derive(Debug, Clone, Default)]
+struct Map<T> {
     n: usize,
-    coordinate_limit: Option<C>,
     data: Vec<T>,
 }
 
-impl<T, C> Default for Map<T, C> {
-    fn default() -> Self {
-        Self { n: 0, coordinate_limit: None, data: Vec::new() }
-    }
-}
-
 #[allow(dead_code)]
-impl<T, C> Map<T, C>
-    where T: Eq + Hash + Default + Clone + Cell, C: Coordinate + Clone + Default {
+impl<T> Map<T>
+    where T: Eq + Hash + Default + Clone + Cell {
     fn new_graph(n: usize) -> Self { Self { n, ..Default::default() } }
-    fn new_grid(coordinate_limit: &C) -> Self {
+    fn new_grid<C: Coordinate>(coordinate_limit: &C) -> Self {
         let n = C::size(coordinate_limit);
         let mut data = vec![T::default(); n];
         for i in C::enum_outer_wall_ids(&coordinate_limit) { data[i] = T::wall(); }
-        Self { n, coordinate_limit: Some(coordinate_limit.clone()), data }
+        Self { n, data }
     }
-    fn clone_with<S: Clone + Default>(&self, fill: &S) -> Map<S, C> {
-        Map::<S, C> { n: self.n, coordinate_limit: self.coordinate_limit.clone(),
-            data: vec![fill.clone(); self.n] }
+    fn clone_with<S: Clone + Default>(&self, fill: &S) -> Map<S> {
+        Map::<S> { n: self.n, data: vec![fill.clone(); self.n] }
     }
     #[inline]
     fn is_enterable(&self, i: usize) -> bool {
@@ -44,36 +36,36 @@ impl<T, C> Map<T, C>
     }
 }
 
-impl<T, C> std::ops::Index<usize> for Map<T, C> {
+impl<T> std::ops::Index<usize> for Map<T> {
     type Output = T;
     #[inline]
     fn index(&self, i: usize) -> &Self::Output { &self.data[i] }
 }
 
-impl<T, C> std::ops::IndexMut<usize> for Map<T, C> {
+impl<T> std::ops::IndexMut<usize> for Map<T> {
     #[inline]
     fn index_mut(&mut self, i: usize) -> &mut Self::Output { &mut self.data[i] }
-}
-
-impl<T, C: Coordinate> std::ops::Index<C> for Map<T, C> {
-    type Output = T;
-    #[inline]
-    fn index(&self, coordinate: C) -> &Self::Output {
-        &self.data[coordinate.to_index(self.coordinate_limit.as_ref().unwrap())]
-    }
-}
-
-impl<T, C: Coordinate> std::ops::IndexMut<C> for Map<T, C> {
-    #[inline]
-    fn index_mut(&mut self, coordinate: C) -> &mut Self::Output {
-        &mut self.data[coordinate.to_index(self.coordinate_limit.as_ref().unwrap())]
-    }
 }
 
 trait Cell {
     fn wall() -> Self;
     fn is_enterable(&self) -> bool;
     fn is_movable(&self) -> bool;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+enum DummyCell {
+    #[default]
+    None,
+}
+
+impl Cell for DummyCell {
+    #[inline]
+    fn wall() -> Self { Self::None }
+    #[inline]
+    fn is_enterable(&self) -> bool { true }
+    #[inline]
+    fn is_movable(&self) -> bool { true }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
@@ -217,10 +209,43 @@ impl Adjacent for Adjacent2D4dir {
     }
 }
 
+struct AdjacentUnDirected {
+    adj: Vec<Vec<(usize, isize)>>,
+}
+
+#[allow(dead_code)]
+impl AdjacentUnDirected {
+    fn new(n: usize, edges: &[(usize, usize)]) -> Self {
+        let mut adj = vec![vec![]; n];
+        for &(a, b) in edges {
+            adj[a].push((b, 1));
+            adj[b].push((a, 1));
+        }
+        Self { adj }
+    }
+    fn new_with_cost(n: usize, edges: &[(usize, usize, isize)]) -> Self {
+        let mut adj = vec![vec![]; n];
+        for &(a, b, c) in edges {
+            adj[a].push((b, c));
+            adj[b].push((a, c));
+        }
+        Self { adj }
+    }
+}
+
+impl Adjacent for AdjacentUnDirected {
+    fn get(&self, i: usize) -> HashSet<usize> {
+        self.adj[i].iter().map(|&(j, _)| j).collect()
+    }
+    fn get_with_cost(&self, i: usize) -> HashSet<(usize, isize)> {
+        self.adj[i].iter().map(|&(j, c)| (j, c)).collect()
+    }
+}
+
 // ダイクストラ法での(距離, dp復元用の1つ前の頂点)を求める
 #[allow(dead_code)]
-fn dijkstra<T, A, C>(start: usize, map: &Map<T, C>, adj: &A) -> Map<(usize, Option<usize>), C>
-        where T: Eq + Hash + Default + Clone + Cell, A: Adjacent, C: Coordinate + Clone + Default {
+fn dijkstra<T, A>(start: usize, map: &Map<T>, adj: &A) -> Map<(usize, Option<usize>)>
+        where T: Eq + Hash + Default + Clone + Cell, A: Adjacent {
     let mut res = map.clone_with(&(INF, None));
     res[start] = (0, None);
     let mut heapq = BinaryHeap::new();
@@ -242,8 +267,8 @@ fn dijkstra<T, A, C>(start: usize, map: &Map<T, C>, adj: &A) -> Map<(usize, Opti
 
 // bfsでの(距離, dp復元用の1つ前の頂点)を求める
 #[allow(dead_code)]
-fn bfs<T, A, C>(start: usize, map: &Map<T, C>, adj: &A) -> Map<(usize, Option<usize>), C>
-        where T: Eq + Hash + Default + Clone + Cell, A: Adjacent, C: Coordinate + Clone + Default {
+fn bfs<T, A>(start: usize, map: &Map<T>, adj: &A) -> Map<(usize, Option<usize>)>
+        where T: Eq + Hash + Default + Clone + Cell, A: Adjacent {
     let mut res = map.clone_with(&(INF, None));
     res[start] = (0, None);
     let mut todo = VecDeque::new();
@@ -272,9 +297,8 @@ mod test {
     #[test]
     fn test_basic_matrix2d() {
         let coordinate_limit = Coordinate2D { x: 3, y: 2 };
-        let mut map =
-            Map::<BasicCell, Coordinate2D>::new_grid(&coordinate_limit);
-        map[Coordinate2D { x: 1, y: 0 }] = BasicCell::Wall;
+        let mut map = Map::<BasicCell>::new_grid(&coordinate_limit);
+        map[Coordinate2D { x: 1, y: 0 }.to_index(&coordinate_limit)] = BasicCell::Wall;
         let start = Coordinate2D { x: 0, y: 0 }.to_index(&coordinate_limit);
         let adj = Adjacent2D4dir::new(&coordinate_limit);
         let res = bfs(start, &map, &adj);
@@ -285,6 +309,14 @@ mod test {
 
     #[test]
     fn test_basic_grid() {
-        //let mut map = Map::<(), ()>::new_graph(n);
+        let map = Map::<DummyCell>::new_graph(6);
+        let start = 0;
+        let edges = vec![(0, 1, 1), (0, 2, 3), (1, 4, 4), (2, 4, 1), (4, 5, 1)];
+        let adj = AdjacentUnDirected::new_with_cost(map.n, &edges);
+        let res = bfs(start, &map, &adj);
+        eprint!("{:?}\n", res.data);
+        let res = dijkstra(start, &map, &adj);
+        eprint!("{:?}\n", res.data);
+        assert!(false);
     }
 }
