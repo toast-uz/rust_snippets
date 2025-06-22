f01ef81b3e92d872ee32962c565197ead2197495
toast-uz <tatsuzo.osawa@gmail.com>
2025-06-22 14:25:02 +0900

    Refactored

--- src/ahc_template.rs
+++ src/ahc_template.rs
@@ -13,34 +13,62 @@ AHC共通テンプレート
 - 問題依存処理と共通処理を分離することで再利用性を高める
 - AHCの実装スタイルを整理し、段階的な改良を促す
 
+# 使用方法
+[1] 自明解を実装する
+[1-1] 入力を受け取る
+[1-2] 入力をもとに初期化する
+[1-3] 状態を定義する（オプショナル）
+[1-4] 操作を定義する（オプショナル）
+[1-5] ソルバーを定義する
+[1-6] 自明解のアルゴリズムを記述する
+[1-7] スコアを計算する
+[1-8] 出力を作成する
+[1-9] 以上を実装したら、SOLVER_INITをTrivialに変更する
+
+[2] 貪欲法を実装する
+[2-1] 貪欲法固有の環境を定義する
+[2-2] 貪欲で最適な操作を選択する
+[2-3] 選択された操作を適用する（操作の選択はここにはいれないこと）
+[2-4] 操作を保存する
+[2-5] 状態を保存する
+[2-6] 以上を実装したら、SOLVER_MAINをGreedyに変更する
+
+/文脈が強ければビームサーチを、文脈が弱ければ焼きなまし法を選択する
+
+[3] 高度なアルゴリズム:ビームサーチを実装する
+[3-1] ビームサーチ固有の環境を定義する
+[3-2] 状態と操作の変換を追加する
+[3-3] 以上を実装したら、SOLVER_MAINをBeamに変更する
+[3-4] ビーム幅を1固定にして、ビームサーチを実行して結果を確認する
+[3-5] ビーム幅を調整する
+
+[4] 高度なアルゴリズム:焼きなまし法を実装する
+[4-1] 焼きなまし固有の環境を定義する
+[4-2] 近傍遷移を定義する
+[4-3] 状態と近傍遷移の変換を定義する
+[4-4] 以上を実装したら、SOLVER_MAINをAnnealに変更する
+
 */
 
 use std::time::Instant;
 use itertools::Itertools;
 use rand::{rngs::SmallRng, Rng, SeedableRng};
-use rustc_hash::FxHashSet as HashSet;
 use proconio::input;
-
-// ----- 問題固有の実装 -----
+// HashSet/HashMapを使う場合は、必ずFxHashを使うこと
+//use rustc_hash::{FxHashSet as HashSet, FxHashMap as HashMap};
 
 const LIMIT: f64 = 0.0; // 制限時間（秒）
 #[allow(dead_code)]
 const DEBUG: bool = true;
-
-// ソルバーの切り替え（実装したら変更する）
-const SOLVER_INIT: SolverState = SolverState::None;  // 初期ソルバー
-const SOLVER_MAIN: SolverState = SolverState::None;  // メインソルバー
-
+const INF: usize = 1 << 60 as usize;
 
 #[allow(unused_macros)]
 macro_rules! dbg {( $( $x:expr ),* ) => ( if DEBUG {eprintln!($( $x ),* );}) }
 
-// --------------------------------------------------------------------
-// [1] 環境を定義して入力を記載する
-// --------------------------------------------------------------------
-
 #[derive(Debug, Clone, Default)]
 struct Env {
+    // スコアの評価方向を追加
+    score_direction: ScoreDirection,
     // 貪欲法用の定義
     max_depth: usize,
     // ビームサーチ用の定義
@@ -60,150 +88,31 @@ struct Env {
 impl Env {
     fn new() -> Self {
         let mut e = Self::default();
-        // 入力を読み込む
+        // [1] 自明解を作成する
+        // [1-1] 入力を受け取る
         input! {
             n: usize,
         }
         e.init(n); e
     }
-    // 入力をもとに初期化する（標準入力をともなわないテスト用に、関数をわけておく）
+    // 標準入力をともなわないテスト用に、関数をわけておく
     fn init(&mut self, _n: usize) {
-        self.init_greedy();     // 貪欲法の初期化
-        self.init_beam();       // ビームサーチの初期化
-        self.init_annealing();  // 焼きなまし法の初期化
-    }
-}
-
-// --------------------------------------------------------------------
-// [2] 自明解を作成する
-// --------------------------------------------------------------------
-
-// [2-1] 状態を定義する（オプショナル）
-#[derive(Debug, Clone, Default)]
-struct State {
-    // 状態を記述する
-    // 例: a: usize, b: usize
-    score: isize,
-}
-
-impl State {
-    fn new(_e: &Env) -> Self {
-        Self::default()
-    }
-}
-
-// [2-2] 操作を定義する（オプショナル）
-#[derive(Debug, Clone, Default)]
-struct Op {
-    // 操作の内容を記述する
-    // 例: a: usize, b: usize
-    pre_id: usize, // 前の状態のID
-    hash: u64, // ハッシュ値
-    score: isize,
-}
-
-impl Op {
-    fn new() -> Self {
-        Self::default()
-    }
-}
-
-// [2-3] ソルバーを定義する
-#[derive(Debug, Clone, Default)]
-struct Solver {
-    // ソルバーの定義を記述する
-    //ops: Vec<Op>, // 操作のリスト
-    //state: State, // 現在の状態
-    counter: usize,
-    score: isize,
-}
-
-impl Solver {
-    fn solve_trivial(&mut self, _e: &Env) {}                    // 自明解のアルゴリズムを記述する
-    fn compute_score(&self, _e: &Env) -> isize { 0 }            // スコアを計算する
-    fn make_output(&self, _e: &Env) -> String { String::new() } // 出力を作成する
-}
-
-// [2-4] 以上を実装したら、SOLVER_INITをTrivialに変更する
-
-// --------------------------------------------------------------------
-// [3] 貪欲法を実装する
-// solve_greedy はなるべく変更しないこと
-// 必要に応じてこれまでの範囲を拡張すること
-// --------------------------------------------------------------------
-
-// [3-1] 貪欲法固有の環境を定義する
-impl Env {
-    fn init_greedy(&mut self) {
-        self.max_depth = 100; // 深さの上限
-    }
-}
-
-// [3-2] シンプルな状態と操作の変換を定義する
-impl State {
-    // 貪欲で最適な操作を選択する
-    fn get_next_greedy_op(&self, _e: &Env) -> Op {
-        // 貪欲法の次の操作を決定する
-        Op::new()
-    }
-    // 選択された操作を適用する
-    fn apply_op(&mut self, _e: &Env, _op: &Op) {
-        // 操作を適用する
-    }
-}
-
-// [3-3] ソルバーに操作と状態を保存するメソッドを追加する
-impl Solver {
-    fn save_ops(&mut self, _e: &Env, _ops: Vec<Op>) {
-        // 操作を登録する
-    }
-    fn save_state(&mut self, _e: &Env, _state: State) {
-        // 状態を登録する
-    }
-}
-
-// [3-4] 以上を実装したら、SOLVER_INITをGreedyに変更する
-
-// --------------------------------------------------------------------
-// [4] 高度なアルゴリズム:ビームサーチを実装する
-// 文脈が強ければビームサーチを、文脈が弱ければ焼きなまし法を選択する
-// --------------------------------------------------------------------
-
-// [4-1] ビームサーチ固有の環境を定義する
-impl Env {
-    fn init_beam(&mut self) {
+        self.score_direction = ScoreDirection::Maximize;
+        // [1] 自明解を作成する
+        // [1-2] 入力をもとに初期化する
+        // 例　self.n = n;;
+        // [2] 貪欲法を実装する
+        // [2-1] 貪欲法固有の環境を定義する
+        // 例　self.max_depth = 100; // 深さの上限
+        // [3] 高度なアルゴリズム:ビームサーチを実装する
+        // [3-1] ビームサーチ固有の環境を定義する
         self.beam_width_init = 1; // ビームの幅（初期値）
         self.beam_width_max = 1; // ビームの幅（最小値）
         self.beam_width_min = 1; // ビームの幅（最大値）
         self.beam_width_min_rate = 0.5; // ビーム幅の変化率（最小値）
         self.beam_width_max_rate = 2.0; // ビーム幅の変化率（最大値）
-    }
-}
-
-// [4-2] 状態と操作の変換を追加する
-impl State {
-    #[inline]
-    // 前の状態のIDを取得する
-    fn get_prev_op(&self) -> Option<&Op> {
-        None
-    }
-    //次の操作を列挙する
-    fn enum_next_ops(&self, _e: &Env, _id: usize) -> Vec<Op> {
-        vec![Op::new()]
-    }
-}
-
-// [4-3] 以上を実装したら、SOLVER_MAINをBeamに変更する
-// [4-4] ビーム幅を1固定にして、ビームサーチを実行して結果を確認する
-// [4-5] ビーム幅を調整する
-
-// --------------------------------------------------------------------
-// [5] 高度なアルゴリズム:焼きなまし法を実装する
-// --------------------------------------------------------------------
-
-// [5-1] 焼きなまし固有の環境を定義する
-impl Env {
-    fn init_annealing(&mut self) {
+        // [4] 高度なアルゴリズム:焼きなまし法を実装する
+        // [4-1] 焼きなまし固有の環境を定義する
         self.sa_start_temp = 1000.0; // 初期温度
         self.sa_end_temp = 1.0;   // 終了温度
         self.sa_timer_resolution = 100; // 焼きなまし法の解像度（温度を変化させるターン数）
@@ -213,17 +122,39 @@ impl Env {
     }
 }
 
-// [5-2] 近傍遷移を定義する
-#[derive(Debug)]
-#[allow(dead_code)]
-enum Transition {
-    // 例: Swap(usize, usize),
-    // 例: Insert(usize, usize),
-    Swap(usize, usize),
+#[derive(Debug, Clone, Default)]
+struct State {
+    // [1] 自明解を作成する
+    // [1-3] 状態を定義する（オプショナル）
+    // 例: a: usize, b: usize
+    pre_op_id: usize, // 前の操作のID
+    score: isize,
 }
 
-// [5-3] 状態と近傍遷移の変換を定義する
 impl State {
+    fn new(_e: &Env, pre_op_id: usize) -> Self {
+        let mut state = Self::default();
+        state.pre_op_id = pre_op_id;
+        state
+    }
+    // [2] 貪欲法を実装する
+    // [2-2] 貪欲で最適な操作を選択する
+    fn get_next_greedy_op(&self, _e: &Env) -> Op {
+        Op::new(self.pre_op_id)
+    }
+    // [2-3] 選択された操作を適用する（操作の選択はここにはいれないこと）
+    fn apply_op(&mut self, _e: &Env, _op: &Op, op_id: usize) {
+        self.pre_op_id = op_id; // 前の操作のIDを更新
+        // 操作を適用する
+    }
+    // [3] 高度なアルゴリズム:ビームサーチを実装する
+    // [3-2] 状態と操作の変換を追加する
+    //次の操作を列挙する
+    fn enum_next_ops(&self, _e: &Env) -> Vec<Op> {
+        vec![Op::new(self.pre_op_id)]
+    }
+    // [4] 高度なアルゴリズム:焼きなまし法を実装する
+    // [4-3] 状態と近傍遷移の変換を定義する
     fn choose_transition(&self, _e: &Env, _rng: &mut impl rand::Rng) -> Transition {
         // 遷移候補を決定する
         Transition::Swap(0, 0)
@@ -246,21 +177,78 @@ impl State {
     }
 }
 
-impl Solver {
-    fn load_state(&mut self, _e: &Env) -> State {
-        // 状態を読み込む
-        State::new(_e)
+#[derive(Debug, Clone, Default)]
+struct Op {
+    // [1] 自明解を作成する
+    // [1-4] 操作を定義する（オプショナル）
+    // 例: a: usize, b: usize
+    pre_id: usize, // 前の状態のID
+    score: isize,
+}
+
+impl Op {
+    fn new(pre_id: usize) -> Self {
+        let mut op = Self::default();
+        op.pre_id = pre_id;
+        op
     }
 }
 
-// [5-4] 以上を実装したら、SOLVER_MAINをAnnealingに変更する
-// [5-5] 焼きなまし法を実行して結果を確認する
-// [5-6] 遷移候補を増やしてみる
-// [5-7] 制限時間を伸ばして結果が改善するなら、差分計算を実装する
+// [1] 自明解を作成する
+// [1-5] ソルバーを定義する
+#[derive(Debug, Clone, Default)]
+struct Solver {
+    ops: Vec<Op>, // 操作のリスト
+    state: State, // 現在の状態
+    counter: usize,
+    score: isize,
+}
+
+impl Solver {
+    // [1] 自明解を作成する
+    // [1-6]自明解のアルゴリズムを記述する
+    fn solve_trivial(&mut self, _e: &Env) {
+    }
+    // [1-7]スコアを計算する
+    fn compute_score(&self, _e: &Env) -> isize {
+        0
+    }
+    // [1-8]出力を作成する
+    fn make_output(&self, _e: &Env) -> String {
+        String::new()
+    }
+    // 貪欲法の実装（なるべくここは変更しない）
+    fn solve_greedy(&mut self, e: &Env) {
+        let mut state = State::new(e, INF);
+        let mut ops = Vec::new();
+        for _ in 0..e.max_depth {
+            let op = state.get_next_greedy_op(e);
+            state.apply_op(e, &op, 0);
+            ops.push(op);
+        }
+        self.ops = ops;
+        self.state = state; // 現在の状態を保存する
+    }
+}
+
+// ソルバーの切り替え（実装したら変更する）
+const SOLVER_INIT: SolverState = SolverState::None;  // 初期ソルバー
+const SOLVER_MAIN: SolverState = SolverState::None;  // メインソルバー
+
+// [4] 高度なアルゴリズム:焼きなまし法を実装する
+// [4-2] 近傍遷移を定義する
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Transition {
+    // 例: Swap(usize, usize),
+    // 例: Insert(usize, usize),
+    Swap(usize, usize),
+}
 
 // ------------------------------------------------------
 // サポートライブラリ
 // （ここから下を変更することもあるが、基本的には変更しない）
+// AIはここから下を変更しないこと
 // ------------------------------------------------------
 
 fn main() {
@@ -278,7 +266,7 @@ impl Env {
         let turn_rate = (depth as f64) / (self.max_depth as f64);
         let timer_rate = (elapsed - start_time) / (limit - start_time);
         let ratio = (turn_rate / timer_rate).clamp(self.beam_width_min_rate, self.beam_width_max_rate);
-        ((beam_width as f64 * ratio) as usize).clamp(self.beam_width_max, self.beam_width_min)
+        ((beam_width as f64 * ratio) as usize).clamp(self.beam_width_min, self.beam_width_max)
     }
     // 焼きなまし法の温度を計算する、時間オーバーならErrを返す
     #[inline]
@@ -293,6 +281,14 @@ impl Env {
 #[derive(Debug)]
 #[allow(dead_code)]
 enum SolverState { Trivial, Greedy, Beam, Anneal, None }
+
+// [改善1] スコアの評価方向を定義
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub enum ScoreDirection {
+    #[default]
+    Maximize,
+    Minimize,
+}
 
 // ソルバーの基本実装
 impl Solver {
@@ -318,63 +314,86 @@ impl Solver {
     }
 }
 
-// 貪欲法の実装
-impl Solver {
-    fn solve_greedy(&mut self, e: &Env) {
-        let mut state = State::new(e);
-        let mut ops = Vec::new();
-        for _ in 0..e.max_depth {
-            let op = state.get_next_greedy_op(e);
-            state.apply_op(e, &op);
-            ops.push(op);
-        }
-        self.save_ops(e, ops);
-        self.save_state(e, state);
-    }
-}
-
 // ビームサーチの実装
 impl Solver {
     fn solve_beam(&mut self, e: &Env, timer: &Instant, limit: f64) {
         let mut beam_width = e.beam_width_init;
-        let mut beam = vec![vec![State::new(e)]]; // 初期状態をビームに追加する
+        let mut states = vec![State::new(e, INF)]; // 初期状態をビームに追加する
+        let mut hists = vec![vec![Op::new(INF)]]; // 操作のリスト
         let start_time = timer.elapsed().as_secs_f64();
         for depth in 0..e.max_depth {
             // ビームの状態列をもとに、次の操作を列挙する
             let mut cands = Vec::new();
-            let mut cands_hash = HashSet::default();
-            for (id, state) in beam[depth].iter().enumerate() {
-                let ops = state.enum_next_ops(e, id);
+            for (state_id, state) in states.iter().enumerate() {
+                let ops = state.enum_next_ops(e);
                 self.counter += ops.len();
-                for op in ops {
-                    if cands_hash.insert(op.hash) { cands.push(op); } // 多様性を高めるためhash値の重複を避ける
-                }
+                cands.extend(ops.into_iter().map(|op| (state_id, op)));
             }
+            // 候補がなければ探索終了
+            if cands.is_empty() { break; }
             // ビーム幅以内の候補を状態に適用して、次の状態列とする
             // select_nth_unstable_by_key は、O(n)であるため、高速である
-            cands.select_nth_unstable_by_key(beam_width, |op| op.score);
-            cands.truncate(beam_width);
-            let new_beam = cands.iter().map(|op| {
-                let mut state = beam[depth][op.pre_id].clone();
-                state.apply_op(e, op);
+            if cands.len() > beam_width {
+                // スコアの評価方向に応じて選択方法を切り替える
+                match e.score_direction {
+                    ScoreDirection::Maximize => {
+                        cands.select_nth_unstable_by_key(beam_width, |(_, op)| -op.score);
+                    }
+                    ScoreDirection::Minimize => {
+                        cands.select_nth_unstable_by_key(beam_width, |(_, op)| op.score);
+                    }
+                }
+                cands.truncate(beam_width);
+            }
+            // 古いstatesをもとに、次の状態列を作成する
+            //   各親stateが何回使われるかカウントし、1回しか使われない場合はcloneを避けることで高速化
+            let mut counts = vec![0; states.len()];
+            for (state_id, _) in &cands {
+                counts[*state_id] += 1;
+            }
+            //   古いstatesをOptionでラップして、ムーブ（所有権の移動）できるようにする
+            let mut old_states: Vec<_> = std::mem::take(&mut states)
+                .into_iter()
+                .map(Some)
+                .collect();
+            states = cands.iter().enumerate().map(|(op_id, (state_id, op))| {
+                let mut state = if counts[*state_id] > 1 {
+                    // 複数回使われる場合はクローン
+                    counts[*state_id] -= 1;
+                    old_states[*state_id].as_ref().unwrap().clone()
+                } else {
+                    // 1回しか使われない場合はムーブ
+                    old_states[*state_id].take().unwrap()
+                };
+                state.apply_op(e, op, op_id);
                 state
             }).collect_vec();
-            beam.push(new_beam);
+            // 操作の履歴を保存する
+            hists.push(cands.into_iter().map(|(_, op)| op).collect_vec());
             // ビーム幅を制限時間とターン消化率を考慮して調整する
             beam_width = e.get_next_beam_width(beam_width, depth, timer, start_time, limit);
             dbg!("#{} counter={}, width={}", depth, self.counter, beam_width);
         }
         // ビームを復元する
-        let mut depth = e.max_depth;
-        let mut best_id = beam[depth].iter().enumerate()
-            .max_by_key(|&(_, state)| state.get_prev_op().unwrap().score).unwrap().0;
-        let mut ops = Vec::new();
-        while let Some(op) = beam[depth][best_id].get_prev_op() {
-            ops.push(op.clone());
-            best_id = op.pre_id;
-            depth -= 1;
-        }
-        self.save_ops(e, ops);
+        let depth = e.max_depth;
+        //   スコアの評価方向に応じて最良解を選択する
+        let best_op = match e.score_direction {
+            ScoreDirection::Maximize => hists[depth].iter().max_by_key(|&op| op.score).unwrap(),
+            ScoreDirection::Minimize => hists[depth].iter().min_by_key(|&op| op.score).unwrap(),
+        };
+        let ops_iter = std::iter::successors(Some((depth, best_op)), |(d, current_op)| {
+            let prev_id = current_op.pre_id;
+            if prev_id == INF {
+                None // ルートに到達したら終了
+            } else {
+                let new_depth = *d - 1;
+                Some((new_depth, &hists[new_depth][prev_id]))
+            }
+        });
+        //   イテレータから操作のリストを作成し、逆順にして保存
+        let mut ops = ops_iter.map(|(_, op)| op.clone()).collect_vec();
+        ops.reverse();
+        self.ops = ops;
     }
 }
 
@@ -383,7 +402,7 @@ impl Solver {
     fn solve_aneal(&mut self, e: &Env, timer: &Instant, limit: f64) {
         let start_time = timer.elapsed().as_secs_f64();
         let mut temp = e.sa_start_temp; // 初期温度
-        let mut state = self.load_state(e); // 初期状態を読み込む
+        let mut state = self.state.clone(); // 初期状態を読み込む
         let mut best_state = state.clone();
         let mut best_counter = self.counter;
         let mut rng = SmallRng::seed_from_u64(1);
@@ -403,21 +422,31 @@ impl Solver {
             // 遷移候補を決めて、遷移した場合のコスト差分を計算する
             let trans = state.choose_transition(e, &mut rng);
             let score_diff = state.compute_score_diff(e, &trans);
-            // スコアが高いほど良い場合
-            // スコアが低いほど良い場合は-score_diff、score_diff <= 0 とする
-            let prob = (score_diff as f64 / temp).exp();
-            if score_diff >= 0 || prob > rng.gen() { // 確率probで遷移する
+            // スコアの評価方向に応じて遷移確率と最良解の更新を切り替える
+            let accept = match e.score_direction {
+                ScoreDirection::Maximize => {
+                    let prob = (score_diff as f64 / temp).exp();
+                    score_diff >= 0 || prob > rng.gen()
+                }
+                ScoreDirection::Minimize => {
+                    let prob = (-score_diff as f64 / temp).exp();
+                    score_diff <= 0 || prob > rng.gen()
+                }
+            };
+            if accept { // 確率probで遷移する
                 state.apply_transition(e, &trans);
                 state.score += score_diff;
-                // スコアが高いほど良い場合
-                // スコアが低いほど良い場合は self.score < best.score とする
-                if best_state.score < state.score {
+                let is_best = match e.score_direction {
+                    ScoreDirection::Maximize => best_state.score < state.score,
+                    ScoreDirection::Minimize => best_state.score > state.score,
+                };
+                if is_best {
                     best_state = state.clone();
                     dbg!("counter:{} score:{} new best", self.counter, best_state.score);
                 }
             }
         }
         // 現在のベストを最終結果として採用する
-        self.save_state(e, best_state);
+        self.state = best_state;
     }
 }
